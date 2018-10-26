3da41aecb62e397fe6c32d45c54f59993470655a
nepeat <nepeat@users.noreply.github.com>
2018-10-26 01:12:51 -0700

    Working bankwatch, in theory.

---
+++ bankwatch/common.py
@@ -0,0 +1,100 @@
+import re
+import hashlib
+import os
+import datetime
+import requests
+
+from plaid import Client as PlaidClient
+
+# Plaid
+plaid_client = PlaidClient(
+    client_id=os.environ["PLAID_CLIENT_ID"],
+    secret=os.environ["PLAID_SECRET"],
+    public_key=os.environ["PLAID_PUBLIC_KEY"],
+    environment=os.environ["PLAID_ENV"]
+)
+
+def get_transactions(item_id, access_token, redis, get_inserted=False):
+    transactions_key = "bank:transactions:" + item_id
+    done = False
+
+    date_from = (datetime.datetime.now() - datetime.timedelta(weeks=4)).strftime("%Y-%m-%d")
+    date_to = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    transactions = []
+    offset = 0
+
+    while not done and len(transactions) <= offset:
+        response = plaid_client.Transactions.get(
+            access_token,
+            start_date=date_from,
+            end_date=date_to,
+            offset=len(transactions),
+            count=500
+        )
+        if offset == 0:
+            offset = response['total_transactions']
+
+        for transaction in response['transactions']:
+            if redis.sismember(transactions_key, transaction["transaction_id"]):
+                done = True
+                if get_inserted:
+                    transactions.append(transaction)
+            else:
+                transactions.append(transaction)
+                
+            redis.sadd(transactions_key, transaction["transaction_id"])
+
+    return transactions
+
+def load_accounts(access_token, redis):
+    for account in plaid_client.Identity.get(access_token)["accounts"]:
+        redis.hset("bank:accounts", account["account_id"], account["name"])
+
+def push_transactions(transactions, redis):
+    total_cash = 0
+    desctext = ""
+
+    account_groups = {}
+
+    # Fill in account groups
+    for transaction in transactions:
+        account_id = transaction["account_id"]
+        clean_name = transaction['name'].replace("**", "\**")
+        total_cash += transaction["amount"]
+
+        if account_id not in account_groups:
+            account_groups[account_id] = ""
+
+        account_groups[account_id] += f"[{transaction['date']}] {clean_name} - **${transaction['amount']}**\n"
+
+    # Replace account IDs with names
+    for raw_account_name in account_groups.copy().keys():
+        account_name = redis.hget("bank:accounts", raw_account_name)
+        if account_name:
+            account_groups[account_name] = account_groups.pop(raw_account_name)
+
+    # Turn accounts into fields
+    account_fields = []
+    for account_name, account_data in account_groups.items():
+        account_fields.append({
+            "name": account_name,
+            "value": account_data.rstrip()
+        })
+
+    r = requests.post(os.environ["DISCORD_WEBHOOK"], json={
+        "embeds": [
+            {
+                # Ass bleach pink
+                "color": 0xffb9ec,
+                "title": "New transactions",
+                "description": desctext,
+                "fields": account_fields,
+                "footer": {
+                    "text": f"${total_cash:.2f} transacted total. Check Waves for a more complete view."
+                }
+            }
+        ]
+    })
+
+    return r.status_code, r.text
