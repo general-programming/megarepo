52a552a7b5c7737dfb560a107ef16b751af6e181
nepeat <nepeat@gmail.com>
2019-03-20 09:44:16 -0700

    Working flow to Checkout.

--- billing/billing-frontend/src/App.js
+++ billing/billing-frontend/src/App.js
@@ -7,17 +7,29 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
+import Home from './components/Home';
 import Payment from './components/Payment';
+
+const stripeBetas = ['checkout_beta_4'];
+let stripeKey = "";
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  stripeKey = "pk_test_s8Uv3fvUXxJfyMVSdVYq4KZK";
+} else {
+  stripeKey = "pk_live_jMaLiRMATTwNk89wb7p4OgUT";
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <StripeProvider apiKey="pk_test_s8Uv3fvUXxJfyMVSdVYq4KZK">
+        <StripeProvider apiKey={stripeKey} betas={stripeBetas}>
           <Elements>
             <Router>
               <Header />
-              <Payment/>
+              { /* "XXX: Handle stripe/success, stripe/canceled" */ }
+              <Route path="/" exact component={Home} />
+              <Route path="/pay" exact component={Payment} />
             </Router>
           </Elements>
         </StripeProvider>
