efc6cb7926f596a55ebd157f072bfdb18ee3d5a2
nepeat <nepeat@gmail.com>
2019-07-04 21:26:10 -0700

    [other] Cisco barcode script

---
+++ scripts/cisco-scanner.py
@@ -0,0 +1,7 @@
+while True:
+    with open("ciscos.csv", "a") as f:
+        model = input("Model: ")
+        serial = input("Serial: ")
+        mac = input("MAC: ")
+        f.write(",".join([model, serial, mac]))
+        f.write("\n")
