c9f6ac3949a5e07a9466e165acddc27a7c28ec10
erin <erin@generalprogramming.org>
2020-04-05 16:04:22 -0700

    Move service submodules to services/

---
+++ services/mailapis/concept.py
@@ -0,0 +1,42 @@
+import os
+import asyncio
+from pyppeteer import launch
+import json
+import datetime
+
+HEIGHT_PADDING = 25
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('file:///tmp/test.html')
+
+    real_height = await page.evaluate('''() => {
+        let body = document.body,
+            html = document.documentElement;
+
+        return Math.max(
+            body.scrollHeight,
+            body.offsetHeight, 
+            html.clientHeight,
+            html.scrollHeight,
+            html.offsetHeight
+        )
+    }''') + HEIGHT_PADDING
+
+    await page.setViewport({
+        "width": 1024,
+        "height": real_height
+    })
+    await page.screenshot({
+        'path': 'example.png',
+        "clip": {
+            "x": 0,
+            "y": 0,
+            "width": 1024,
+            "height": real_height
+        }
+    })
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
