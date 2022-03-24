2d6718aa3a492256dae9536ce32c7f05b3758556
nepeat <nepeat@gmail.com>
2022-03-24 19:10:39 +0000

    automation - remove hacky dpkg_waitlock

--- automation/deploy_server/common/00-install-packages.sh
+++ automation/deploy_server/common/00-install-packages.sh
@@ -1,35 +1,26 @@
 #!/bin/sh
 set -xe
 
-# Shared
-dpkg_waitlock () {
-    while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-        sleep 1
-    done
-
-    eval "$@"
-}
-
 export DEBIAN_FRONTEND=noninteractive
 
 # Update apt sources and install latest packages.
-dpkg_waitlock apt-get update
-dpkg_waitlock apt-get -y dist-upgrade
+apt-get -o DPkg::Lock::Timeout=-1 update
+apt-get -o DPkg::Lock::Timeout=-1 -y dist-upgrade
 
 # Install software-properties-common and other apt goodies.
-dpkg_waitlock apt-get -y install apt-transport-https gnupg-agent software-properties-common ca-certificates curl
+apt-get -o DPkg::Lock::Timeout=-1 -y install apt-transport-https gnupg-agent software-properties-common ca-certificates curl
 
 # Install HashiCorp repo
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
-dpkg_waitlock apt-get update
-dpkg_waitlock apt-get -y install nomad vault consul
+apt-get -o DPkg::Lock::Timeout=-1 update
+apt-get -o DPkg::Lock::Timeout=-1 -y install nomad vault consul
 
 # Install Ansible repo
 add-apt-repository --yes --update ppa:ansible/ansible
 
 # Install our packages
-dpkg_waitlock apt-get -y install fail2ban docker.io mosh python3-virtualenv ndppd git mosh traceroute htop cloud-init byobu ansible
+apt-get -o DPkg::Lock::Timeout=-1 -y install fail2ban docker.io mosh python3-virtualenv ndppd git mosh traceroute htop cloud-init byobu ansible
 
 # Install pyinfra in /root
 virtualenv -p python3 /root/pyenv
