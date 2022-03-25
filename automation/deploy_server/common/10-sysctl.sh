724aa0a064833d737267ba6d35c9c08afe6c053b
nepeat <nepeat@gmail.com>
2022-03-25 03:06:22 +0000

    automation - include ip forward enable in sysctl

--- automation/deploy_server/common/10-sysctl.sh
+++ automation/deploy_server/common/10-sysctl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo 'net.ipv4.conf.all.forwarding=1' > /etc/sysctl.d/99-enable-ipv4-forwarding.conf
+echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/99-enable-ipv4-forwarding.conf
+echo 'net.ipv4.conf.all.forwarding=1' >> /etc/sysctl.d/99-enable-ipv4-forwarding.conf
 echo 'vm.swappiness=1' > /etc/sysctl.d/99-swappiness.conf
 
 sysctl --system
