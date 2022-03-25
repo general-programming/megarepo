3e1af63c555b4378184f64ec5ba7d2a172f73a03
nepeat <nepeat@gmail.com>
2022-03-25 03:06:04 +0000

    automation - combine nasty hack with flag

--- automation/deploy_server/common/00-install-packages.sh
+++ automation/deploy_server/common/00-install-packages.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 set -xe
 
+# Shared
+dpkg_waitlock () {
+    while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+        sleep 1
+    done
+
+    eval "$@"
+}
+
 export DEBIAN_FRONTEND=noninteractive
 
 # Update apt sources and install latest packages.
-apt-get -o DPkg::Lock::Timeout=-1 update
-apt-get -o DPkg::Lock::Timeout=-1 -y dist-upgrade
+dpkg_waitlock apt-get -o DPkg::Lock::Timeout=-1 update
+dpkg_waitlock apt-get -o DPkg::Lock::Timeout=-1 -y dist-upgrade
 
 # Install software-properties-common and other apt goodies.
-apt-get -o DPkg::Lock::Timeout=-1 -y install apt-transport-https gnupg-agent software-properties-common ca-certificates curl
+dpkg_waitlock apt-get -o DPkg::Lock::Timeout=-1 -y install apt-transport-https gnupg-agent software-properties-common ca-certificates curl
 
 # Install HashiCorp repo
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
-apt-get -o DPkg::Lock::Timeout=-1 update
-apt-get -o DPkg::Lock::Timeout=-1 -y install nomad vault consul
+dpkg_waitlock apt-get -o DPkg::Lock::Timeout=-1 update
+dpkg_waitlock apt-get -o DPkg::Lock::Timeout=-1 -y install nomad vault consul
 
 # Install Ansible repo
-add-apt-repository --yes --update ppa:ansible/ansible
+dpkg_waitlock add-apt-repository --yes --update ppa:ansible/ansible
 
 # Install our packages
-apt-get -o DPkg::Lock::Timeout=-1 -y install fail2ban docker.io mosh python3-virtualenv ndppd git mosh traceroute htop cloud-init byobu ansible
+dpkg_waitlock apt-get -o DPkg::Lock::Timeout=-1 -y install fail2ban docker.io mosh python3-virtualenv ndppd git mosh traceroute htop cloud-init byobu ansible
 
 # Install pyinfra in /root
 virtualenv -p python3 /root/pyenv
