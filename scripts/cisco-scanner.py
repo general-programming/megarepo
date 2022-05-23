bc65ccba7d3925a484120a159351139166c09090
nepeat <nepeat@gmail.com>
2022-05-23 03:00:04 -0700

    chore: remove cisco-scanner.py

--- scripts/cisco-scanner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-while True:
-    with open("ciscos.csv", "a") as f:
-        model = input("Model: ")
-        serial = input("Serial: ")
-        mac = input("MAC: ")
-        f.write(",".join([model, serial, mac]))
-        f.write("\n")
