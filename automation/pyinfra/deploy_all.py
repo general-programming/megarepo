f71b4640a8f6cadda8eccc27362b52f02b3208c7
nepeat <nepeat@gmail.com>
2022-10-30 04:09:21 -0700

    fix: run setup_base for all hosts

--- automation/pyinfra/deploy_all.py
+++ automation/pyinfra/deploy_all.py
@@ -1,5 +1,6 @@
 from pyinfra import local
 
+local.include("tasks/setup_base.py")
 local.include("tasks/add_ssh_admins.py")
 local.include("tasks/setup_holepunch.py")
 local.include("tasks/setup_docker_v6.py")
