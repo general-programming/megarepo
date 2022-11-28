75b48ea3cab318b8c48dd642fd797372f09151c3
nepeat <nepeat@gmail.com>
2022-11-27 22:48:03 -0800

    feat: archive team golang tracker streamer

---
+++ projects/go-at-logstream/go.mod
@@ -0,0 +1,17 @@
+module github.com/general-programming/megarepo/projects/go-at-logstream
+
+go 1.19
+
+require github.com/bytedance/sonic v1.5.0
+
+require (
+	github.com/chenzhuoyu/base64x v0.0.0-20211019084208-fb5309c8db06 // indirect
+	github.com/googollee/go-socket.io v0.9.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
+)
---
+++ projects/go-at-logstream/main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/bytedance/sonic"
+	socketio "github.com/googollee/go-socket.io"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type ATProjects struct {
+	WarriorProject string      `json:"auto_project"`
+	Projects       []ATProject `json:"projects"`
+}
+
+type ATProject struct {
+	Description     string `json:"description"`
+	ProjectName     string `json:"name"`
+	Title           string `json:"title"`
+	Repository      string `json:"repository"`
+	LeaderboardLink string `json:"leaderboard"`
+}
+
+type ATTrackerUpdate struct {
+	Project    string `json:"project"`
+	Downloader string `json:"downloader"`
+
+	Bytes       int64 `json:"bytes"`
+	Valid       bool  `json:"valid"`
+	IsDuplicate bool  `json:"is_duplicate"`
+
+	Items     []string `json:"items"`
+	MoveItems []string `json:"move_items"`
+
+	Item     string        `json:"item"`
+	ItemRTTs []json.Number `json:"item_rtts"`
+
+	WarriorUserAgent string `json:"user_agent"`
+	WarriorVersion   string `json:"version"`
+
+	SizeMB json.Number `json:"megabytes"`
+}
+
+func FetchProjects() *ATProjects {
+	resp, err := http.Get("https://warriorhq.archiveteam.org/projects.json")
+	if err != nil {
+		panic("Failed to fetch projects")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic("Failed to read response body")
+	}
+
+	result := &ATProjects{}
+	err = sonic.Unmarshal(body, result)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal response body: %v", err))
+	}
+
+	return result
+}
+
+func GetLogSocketNameFromLeaderboard(link string) string {
+	// parses the final bit of the leaderboard link to get the project name
+	// e.g. "https://tracker.archiveteam.org/grabtemp20221126" -> "grabtemp20221126"
+
+	// this is done by getting the last part of the link, and removing the query parameters
+	// (if there are any)
+	split := strings.Split(link, "/")
+	last := split[len(split)-1]
+	if strings.Contains(last, "?") {
+		split = strings.Split(last, "?")
+		return split[0]
+	}
+
+	return last
+}
+
+func logInit(d bool) *zap.Logger {
+
+	pe := zap.NewProductionEncoderConfig()
+
+	// fileEncoder := zapcore.NewJSONEncoder(pe)
+
+	pe.EncodeTime = zapcore.ISO8601TimeEncoder
+	consoleEncoder := zapcore.NewConsoleEncoder(pe)
+
+	level := zap.InfoLevel
+	if d {
+		level = zap.DebugLevel
+	}
+
+	core := zapcore.NewTee(
+		// zapcore.NewCore(fileEncoder, zapcore.AddSync(f), level),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), level),
+	)
+
+	l := zap.New(core)
+
+	return l
+}
+
+func OpenLogSocket(project string) {
+	client, err := socketio.Dial("http://tracker.archiveteam.org:8080/" + project + "-log")
+	logger := logInit(false)
+	defer logger.Sync()
+
+	if err != nil {
+		logger.Error("Failed to connect to socket",
+			zap.String("project", project),
+			zap.Error(err),
+		)
+		return
+	}
+
+	client.On("connect", func(ns *socketio.NameSpace) {
+		logger.Info("Connected to socket", zap.String("endpoint", ns.Endpoint()))
+	})
+
+	client.On("log_message", func(ns *socketio.NameSpace, message string) {
+		parsed := &ATTrackerUpdate{}
+		err := sonic.UnmarshalString(message, parsed)
+		if err != nil {
+			logger.Error("Failed to unmarshal message", zap.String("msg", message), zap.Error(err))
+			return
+		}
+
+		// create item text
+		var itemsText string
+		if len(parsed.Items) > 1 {
+			itemsText = fmt.Sprintf("%d items", len(parsed.Items))
+		} else {
+			itemsText = parsed.Item
+		}
+
+		// create size text
+		size, _ := parsed.SizeMB.Float64()
+		sizeText := fmt.Sprintf("%.2fMB", size)
+
+		logger.Info("log_message",
+			zap.String("project", parsed.Project),
+			zap.String("downloader", parsed.Downloader),
+			zap.String("items", itemsText),
+			zap.String("size", sizeText),
+		)
+	})
+
+	client.Run()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	projects := FetchProjects()
+	println(len(projects.Projects), "projects")
+
+	counter := 0
+	for _, project := range projects.Projects {
+		println("Opening socket for", project.ProjectName)
+		wg.Add(1)
+		go func(project ATProject) {
+			defer wg.Done()
+			OpenLogSocket(GetLogSocketNameFromLeaderboard(project.LeaderboardLink))
+		}(project)
+		counter += 1
+	}
+
+	wg.Wait()
+}
