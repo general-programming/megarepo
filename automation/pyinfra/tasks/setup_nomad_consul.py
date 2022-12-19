a07c99868d66d194632e5bdd0bef33b8d1aa89a5
nepeat <nepeat@gmail.com>
2022-12-18 18:14:49 -0800

    feat: very fucky way to detect if the node is in the ephermeral subnet

--- automation/pyinfra/tasks/setup_nomad_consul.py
+++ automation/pyinfra/tasks/setup_nomad_consul.py
@@ -3,7 +3,27 @@ import json
 import os
 
 from pyinfra import host
+from pyinfra.facts.hardware import NetworkDevices
 from pyinfra.operations import files, server, systemd
+
+CLUSTER_CIDR = ipaddress.ip_network("10.101.0.0/24")
+in_cluster_cidr = False
+
+# Get the CIDR for the cluster network.
+for dev, dev_config in host.get_fact(NetworkDevices).items():
+    # Easiest: Check if the device is in netmaker.
+    if dev == "nm-netmaker":
+        in_cluster_cidr = True
+        break
+
+    # Check if the device is in the cluster CIDR if that fails..
+    if dev_config["ipv4"]:
+        dev_v4_addr = ipaddress.IPv4Address(dev_config["ipv4"]["address"])
+
+        # Check if the CIDR is 10.101.0.0/24
+        if dev_v4_addr in CLUSTER_CIDR:
+            in_cluster_cidr = True
+            break
 
 files.template(
     name="Create Consul config.",
@@ -12,6 +32,7 @@ files.template(
     mode="644",
     consul_datacenter=host.data.consul_datacenter,
     consul_servers=json.dumps(host.data.consul_servers),
+    in_cluster_cidr=in_cluster_cidr,
 )
 
 files.put(
