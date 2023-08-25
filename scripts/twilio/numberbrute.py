6ee697262bcdf456632a30057ac3d30d71a069fe
nepeat <nepeat@gmail.com>
2023-08-25 02:42:24 -0700

    feat: numberbrute change to read from files

--- scripts/twilio/numberbrute.py
+++ scripts/twilio/numberbrute.py
@@ -29,14 +29,14 @@ def get_countries():
     return result
 
 
-def map_to_keypad(input: str) -> str:
-    input = input.lower()
+def map_to_keypad(number: str) -> str:
+    number = number.lower()
     result = ""
 
-    if input.isdecimal():
-        return input
+    if number.isdecimal():
+        return number
 
-    for char in input:
+    for char in number:
         if char.isdecimal():
             result += char
         elif char in PHONE_NUMBERPAD_MAP:
@@ -47,24 +47,30 @@ def map_to_keypad(input: str) -> str:
     return result
 
 
-# countries = ["US"]
-countries = get_countries()
-thing_to_search = map_to_keypad(sys.argv[1])
-print(thing_to_search)
+if os.path.exists(sys.argv[1]):
+    countries = ["US"]
+    with open(sys.argv[1]) as f:
+        numbers = [map_to_keypad(line.strip()) for line in f.readlines()]
+    print(numbers)
+else:
+    countries = get_countries()
+    numbers = [map_to_keypad(sys.argv[1])]
 
-for country in countries:
-    print(country)
+for number in numbers:
+    for country in countries:
+        print("checking", country, number)
 
-    try:
-        numbers = twilio_client.available_phone_numbers(country).local.list(
-            beta=True, contains=thing_to_search
-        )
-    except twilio.base.exceptions.TwilioException as e:
-        print(e)
-        continue
+        try:
+            numbers = twilio_client.available_phone_numbers(country).local.list(
+                beta=True,
+                contains=number,
+            )
+        except twilio.base.exceptions.TwilioException as e:
+            print(e)
+            continue
 
-    for number in numbers:
-        print(number.friendly_name)
+        for number in numbers:
+            print("available", number.friendly_name)
 
 
 # twilio.
